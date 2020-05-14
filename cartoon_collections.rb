8a68a15cc05b7a5b728eac727d743cb931e72b8a
Adam Altmark <adamaltmark@gmail.com>
2020-05-14 14:19:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,8 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.any? do |element|
+  valid_calls.join(" ",)
+planeteer_calls.any? do |element|
     element.include?(valid_calls)
   end
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
